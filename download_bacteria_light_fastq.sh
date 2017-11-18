455c8208ae8c05a4ccc7192dad7a8b07a05290e5
Jason Stajich <jason.stajich@ucr.edu>
2017-11-18 01:57:58 -0800

    fastq download

---
+++ download_bacteria_light_fastq.sh
@@ -0,0 +1,8 @@
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR323/009/SRR3234519/SRR3234519_2.fastq.gz
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR323/009/SRR3234519/SRR3234519_1.fastq.gz
+
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR323/002/SRR3234522/SRR3234522_1.fastq.gz
+curl -O ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR323/002/SRR3234522/SRR3234522_2.fastq.gz
+
+curl -O ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/011/465/GCF_000011465.1_ASM1146v1/GCF_000011465.1_ASM1146v1_genomic.fna.gz
+curl -O ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/011/465/GCF_000011465.1_ASM1146v1/GCF_000011465.1_ASM1146v1_genomic.gff.gz
